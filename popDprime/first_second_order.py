37fe50d7127b38e4ce2baded714413363409a8dd
crheller <charlieheller95@gmail.com>
2020-11-01 14:22:08 -0800

    add options for first/second order figure

--- popDprime/first_second_order.py
+++ popDprime/first_second_order.py
@@ -14,6 +14,12 @@ mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['font.size'] = 6
 
 savefig = True
+group = True
+scatterplot = True
+ms = 30
+ec='k'
+m = 4 #15
+mi = -1 #-4
 
 figsave = DIR + 'results/figures/first_second_order.pdf'
 
@@ -28,9 +34,6 @@ cmap = {
 
 val = 'dp_opt'
 df[val] = np.sqrt(df[val])
-m = 4
-mi = -1
-ms = 30
 
 tar_mask = (df.tar_tar) & (df.tdr_overall==True) & (~df.pca) & df.batch.isin([324, 325]) & (df.f1 == df.f2) 
 cat_mask = (df.cat_tar) & (df.tdr_overall==True) & (~df.pca) & df.batch.isin([324, 325]) & (df.f1 == df.f2) 
@@ -54,8 +57,12 @@ for area in ['A1', 'PEG']:
             n = (act + pas)
         else:
             n = 1
-        delta[area][category] = ((act - pas) / n).groupby(level=0).mean().values
-        sem[area][category] =  ((act - pas) / n).groupby(level=0).sem().values
+        if group:
+            delta[area][category] = ((act - pas) / n).groupby(level=0).mean().values
+            sem[area][category] =  ((act - pas) / n).groupby(level=0).sem().values
+        else:
+            delta[area][category] = ((act - pas) / n).values
+            sem[area][category] =  ((act - pas) / n).values
 
         # sim1 data
         act = df[mask & df.active & (df.area==area) & df.sim1][[val, 'site']].set_index('site')
@@ -64,30 +71,83 @@ for area in ['A1', 'PEG']:
             n = (act + pas)
         else:
             n = 1
-        delta_sim[area][category] = ((act - pas) / n).groupby(level=0).mean().values
-        sem_sim[area][category] =  ((act - pas) / n).groupby(level=0).sem().values
+        if group:
+            delta_sim[area][category] = ((act - pas) / n).groupby(level=0).mean().values
+            sem_sim[area][category] =  ((act - pas) / n).groupby(level=0).sem().values
+        else:
+            delta_sim[area][category] = ((act - pas) / n).values
+            sem_sim[area][category] =  ((act - pas) / n).values
 
-f, ax = plt.subplots(2, 1, figsize=(2, 4))
+if scatterplot:
+    f, ax = plt.subplots(2, 1, figsize=(2, 4))
 
-# ==== A1 ===== 
-ax[0].scatter(delta_sim['A1']['tt'], delta['A1']['tt'], s=ms, color=cmap['tar_tar'], edgecolor='k', label='Tar vs. Tar')
-ax[0].scatter(delta_sim['A1']['ct'], delta['A1']['ct'], s=ms, color=cmap['cat_tar'], edgecolor='k', label='Tar vs. Tar')
-ax[0].scatter(delta_sim['A1']['rr'], delta['A1']['rr'], s=ms, color=cmap['ref_ref'], edgecolor='k', label='Ref vs. Ref')
-ax[0].set_xlabel(r"$\Delta d'$ 1st-order")
-ax[0].set_ylabel(r"$\Delta d'$ Raw")
-ax[0].plot([mi, m], [mi, m], '--', color='grey', lw=2)
-ax[0].set_title('A1')
-ax[0].legend(frameon=False)
+    # ==== A1 ===== 
+    ax[0].scatter(delta_sim['A1']['tt'], delta['A1']['tt'], s=ms, color=cmap['tar_tar'], edgecolor=ec, label='Tar vs. Tar')
+    ax[0].scatter(delta_sim['A1']['ct'], delta['A1']['ct'], s=ms, color=cmap['cat_tar'], edgecolor=ec, label='Cat vs. Tar')
+    ax[0].scatter(delta_sim['A1']['rr'], delta['A1']['rr'], s=ms, color=cmap['ref_ref'], edgecolor=ec, label='Ref vs. Ref', zorder=-3)
+    ax[0].set_xlabel(r"$\Delta d'$ 1st-order")
+    ax[0].set_ylabel(r"$\Delta d'$ Raw")
+    ax[0].plot([mi, m], [mi, m], '--', color='grey', lw=2)
+    ax[0].axhline(0, linestyle='--', color='grey', lw=2, zorder=-4)
+    ax[0].axvline(0, linestyle='--', color='grey', lw=2, zorder=-4)
+    ax[0].set_title('A1')
+    ax[0].legend(frameon=False)
 
-# ==== PEG ===== 
-ax[1].scatter(delta_sim['PEG']['tt'], delta['PEG']['tt'], s=ms, color=cmap['tar_tar'], edgecolor='k', label='Tar vs. Tar')
-ax[1].scatter(delta_sim['PEG']['ct'], delta['PEG']['ct'], s=ms, color=cmap['cat_tar'], edgecolor='k', label='Tar vs. Tar')
-ax[1].scatter(delta_sim['PEG']['rr'], delta['PEG']['rr'], s=ms, color=cmap['ref_ref'], edgecolor='k', label='Ref vs. Ref')
-ax[1].set_xlabel(r"$\Delta d'$ 1st-order")
-ax[1].set_ylabel(r"$\Delta d'$ Raw")
-ax[1].plot([mi, m], [mi, m], '--', color='grey', lw=2)
-ax[1].set_title('PEG')
+    # ==== PEG ===== 
+    ax[1].scatter(delta_sim['PEG']['tt'], delta['PEG']['tt'], s=ms, color=cmap['tar_tar'], edgecolor=ec, label='Tar vs. Tar')
+    ax[1].scatter(delta_sim['PEG']['ct'], delta['PEG']['ct'], s=ms, color=cmap['cat_tar'], edgecolor=ec, label='Cat vs. Tar')
+    ax[1].scatter(delta_sim['PEG']['rr'], delta['PEG']['rr'], s=ms, color=cmap['ref_ref'], edgecolor=ec, label='Ref vs. Ref', zorder=-3)
+    ax[1].set_xlabel(r"$\Delta d'$ 1st-order")
+    ax[1].set_ylabel(r"$\Delta d'$ Raw")
+    ax[1].plot([mi, m], [mi, m], '--', color='grey', lw=2)
+    ax[1].axhline(0, linestyle='--', color='grey', lw=2, zorder=-4)
+    ax[1].axvline(0, linestyle='--', color='grey', lw=2, zorder=-4)
+    ax[1].set_title('PEG')
 
-f.tight_layout()
+    f.tight_layout()
+
+else:
+    # barplot
+    f, ax = plt.subplots(2, 1, figsize=(2, 4), sharey=True, sharex=True)
+
+    ax[0].bar([0, 1, 3, 4, 6, 7],
+              [delta['A1']['rr'].mean(), delta_sim['A1']['rr'].mean(),
+              delta['A1']['ct'].mean(), delta_sim['A1']['ct'].mean(),
+              delta['A1']['tt'].mean(), delta_sim['A1']['tt'].mean()],
+              yerr=[delta['A1']['rr'].std() / np.sqrt(len(delta['A1']['rr'])), delta_sim['A1']['rr'].std() / np.sqrt(len(delta['A1']['rr'])),
+              delta['A1']['ct'].std() / np.sqrt(len(delta['A1']['ct'])), delta_sim['A1']['ct'].std() / np.sqrt(len(delta['A1']['ct'])),
+              delta['A1']['tt'].std() / np.sqrt(len(delta['A1']['tt'])), delta_sim['A1']['tt'].std() / np.sqrt(len(delta['A1']['tt']))],
+              edgecolor=[cmap['ref_ref'], cmap['ref_ref'],
+                        cmap['cat_tar'], cmap['cat_tar'],
+                        cmap['tar_tar'], cmap['tar_tar']],
+            facecolor='none',
+            capsize=3, lw=2)
+    ax[0].set_title('A1')
+    ax[0].set_ylabel(r"$\Delta d'$")
+
+    ax[1].bar([0, 1, 3, 4, 6, 7],
+            [delta['PEG']['rr'].mean(), delta_sim['PEG']['rr'].mean(),
+            delta['PEG']['ct'].mean(), delta_sim['PEG']['ct'].mean(),
+            delta['PEG']['tt'].mean(), delta_sim['PEG']['tt'].mean()],
+            yerr=[delta['PEG']['rr'].std() / np.sqrt(len(delta['PEG']['rr'])), delta_sim['PEG']['rr'].std() / np.sqrt(len(delta['PEG']['rr'])),
+            delta['PEG']['ct'].std() / np.sqrt(len(delta['PEG']['ct'])), delta_sim['PEG']['ct'].std() / np.sqrt(len(delta['PEG']['ct'])),
+            delta['PEG']['tt'].std() / np.sqrt(len(delta['PEG']['tt'])), delta_sim['PEG']['tt'].std() / np.sqrt(len(delta['PEG']['tt']))],
+            edgecolor=[cmap['ref_ref'], cmap['ref_ref'],
+                    cmap['cat_tar'], cmap['cat_tar'],
+                    cmap['tar_tar'], cmap['tar_tar']],
+        facecolor='none',
+        capsize=3, lw=2)
+    ax[1].set_title('PEG')
+    ax[1].set_ylabel(r"$\Delta d'$")
+
+    custom_lines = [Line2D([0], [0], color=cmap['ref_ref'], lw=1),
+                Line2D([0], [0], color=cmap['tar_tar'], lw=1),
+                Line2D([0], [0], color=cmap['cat_tar'], lw=1)]
+    ax[0].legend(custom_lines, ['Ref vs. Ref', 'Tar vs. Tar', 'Cat vs. Tar'], frameon=False)
+
+    f.tight_layout()
+
+if savefig:
+    f.savefig(DIR + '/results/figures/first_second_order.pdf')
 
 plt.show()
